9f05c9612f044ea04d6cada4c9d9e3f42d7a6722
Jose Rivera <ing.joserivera26@gmail.com>
2019-09-26 16:43:33 -0600

    Prepare VPS script for RC versions

--- vps.sh
+++ vps.sh
@@ -21,9 +21,13 @@ fi
 yum clean all
 rm -rf /var/cache/yum
 
-#Download the beta repo of VitalPBX
+#Download VitalPBX's repo
 rm -rf /etc/yum.repos.d/vitalpbx.repo
 wget -P /etc/yum.repos.d/ https://raw.githubusercontent.com/VitalPBX/VPS/master/resources/vitalpbx.repo
+
+#Download VitalPBX's RC repo
+rm -rf /etc/yum.repos.d/vitalpbxrc.repo
+wget -P /etc/yum.repos.d/ http://repo.vitalpbx.org/vitalpbx/rc/vitalpbxrc.repo
 
 #Install SSH Welcome Banner
 rm -rf /etc/profile.d/vitalwelcome.sh
@@ -52,12 +56,12 @@ yum -y install $(cat pack_list)
 # Install VitalPBX
 mkdir -p /etc/ombutel
 mkdir -p /etc/asterisk/ombutel
-yum -y install vitalpbx vitalpbx-asterisk-configs vitalpbx-fail2ban-config vitalpbx-sounds vitalpbx-themes dahdi-linux dahdi-tools dahdi-tools-doc kmod-dahdi-linux fxload
+yum -y install vitalpbx vitalpbx-asterisk-configs vitalpbx-fail2ban-config vitalpbx-sounds vitalpbx-themes
 
 # Speed up the localhost name resolving
 sed -i 's/^hosts.*$/hosts:      myhostname files dns/' /etc/nsswitch.conf
 
-cat << EOF >> /etc/sysctl.d/10-ombutel.conf
+cat << EOF >> /etc/sysctl.d/10-vitalpbx.conf
 # Reboot machine automatically after 20 seconds if it kernel panics
 kernel.panic = 20
 EOF
