b8bf1b8e52ccb9baf5c5297d2ee17c7213bd2f87
Jose Rivera <ing.joserivera26@gmail.com>
2021-02-10 16:46:25 -0600

    Normalize the VPS script
    The new version of GLIBC libraries fixes the issue related to Asterisk conditionals

--- vps.sh
+++ vps.sh
@@ -52,9 +52,6 @@ yum -y install $(cat pack_list)
 systemctl enable firewalld
 systemctl start firewalld
 
-#Downgrade some packages to avoid asterisk issues
-yum downgrade glibc-2.17-317.el7 glibc-common-2.17-317.el7 glibc-devel-2.17-317.el7 glibc-headers-2.17-317.el7 -y
-
 # Clean Cache Again
 yum clean all
 rm -rf /var/cache/yum
@@ -64,8 +61,8 @@ mkdir -p /etc/vitalpbx
 mkdir -p /etc/asterisk/vitalpbx
 yum -y install vitalpbx vitalpbx-asterisk-configs vitalpbx-fail2ban-config vitalpbx-sounds vitalpbx-themes
 
-# Do a full update and skip some packages
-yum update -x glibc,glibc-common,glibc-devel,glibc-headers -y
+# Do a full update
+yum update -y
 
 # Speed up the localhost name resolving
 sed -i 's/^hosts.*$/hosts:      myhostname files dns/' /etc/nsswitch.conf
